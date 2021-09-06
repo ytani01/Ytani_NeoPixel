135b5023e572b2386754001492824f3b0fa26b23
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-09-07 03:41:40 +0900

    START: 0.0.9

--- ytani_neopixel/__init__.py
+++ ytani_neopixel/__init__.py
@@ -5,7 +5,7 @@
 ytani_neopixel
 """
 __prog_name__ = 'ytani-neopixel'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __author__ = 'Yoichi Tanibayashi'
 
 from .ytani_neopixel import NeoPixel, NeoPixel_Color
