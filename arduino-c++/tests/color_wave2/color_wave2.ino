d110bd542d10045a4ac1103a11a37f9651b596a7
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-09-19 05:25:14 +0900

    UPD: color_wave2.ino

--- arduino-c++/tests/color_wave2/color_wave2.ino
+++ arduino-c++/tests/color_wave2/color_wave2.ino
@@ -1,13 +1,13 @@
-#include <Adafruit_NeoPixel.h>
+#include "Ytani_NeoPixel.h"
 
-const uint8_t PIN_ONBOARD_LED = 1;
+const uint8_t PIN_ONBOARD_LED = 1;  // DigiSpark ATTiny85
+//const uint8_t PIN_ONBOARD_LED = 13;  // Arduino Uno
 
-const uint8_t PIN_PIXEL       = 4;
-const uint8_t PIXELS_N        = 16;
-const int     BRIGHTNESS      = 32; // 0-255
-const int     STEP            = BRIGHTNESS / 4;
+const uint8_t PIN_PIXEL        = 4;
+const uint8_t PIXELS_N         = 16;
+const int     BRIGHTNESS       = 32;  // 1-255
 
-const int     LOOP_DELAY      = 100;  // ms
+const unsigned long LOOP_DELAY = 100;  // ms
 
 const uint8_t COLOR_LIST[][3] =
   {
@@ -24,91 +24,60 @@ const uint8_t COLOR_LIST[][3] =
    {BRIGHTNESS, 0, BRIGHTNESS},
    {BRIGHTNESS, 0, int(1*BRIGHTNESS/8)}
   };
+const int COLOR_N = sizeof(COLOR_LIST) / sizeof(COLOR_LIST[0]);
 
-const int COLOR_N = 12;
+Ytani_NeoPixel pixels(PIXELS_N, PIN_PIXEL);
 
 /**
  *
  */
-class NeoPixel {
-public:
-  NeoPixel(uint8_t n, uint8_t pin) {
-    this->_pixels = Adafruit_NeoPixel(n, pin, NEO_GRB + NEO_KHZ800);
-    this->_pixels.begin();
-    this->_pixels.setBrightness(255);
+void set_color_and_show(int color_i, unsigned long delay_msec=0) {
+  if (color_i >= COLOR_N) {
+    color_i %= COLOR_N;
   }
 
-  void clear() {
-    this->_pixels.clear();
+  for (int i=0; i < PIXELS_N; i++) {
+    pixels.setColor(i,
+                    COLOR_LIST[color_i][0],
+                    COLOR_LIST[color_i][1],
+                    COLOR_LIST[color_i][2]);
+
+    color_i = (color_i + 1) % COLOR_N;
+
+    if ( delay_msec > 0 ) {
+      pixels.show();
+      delay(delay_msec);
+    }
+  } // for(i)
+
+  if (delay_msec == 0) {
+    pixels.show();
   }
-
-  void show() {
-    this->_pixels.show();
-  }
-
-  void setColor(int i, uint32_t col) {
-    this->_pixels.setPixelColor(i, col);
-  }
-
-  uint32_t getColor(int i) {
-    return this->_pixels.getPixelColor(i);
-  }
-
-  uint32_t color(uint8_t r, uint8_t g, uint8_t b) {
-    return this->_pixels.Color(r, g, b);
-  }
-
-private:
-  Adafruit_NeoPixel _pixels;
-  uint8_t pixels_n = 0;
-  uint8_t pin;
-};
-
-NeoPixel pixels(PIXELS_N, PIN_PIXEL);
-
-int color_i;
+}
 
 /**
  *
  */
 void setup() {
-  uint32_t col;
-
   pinMode(PIN_ONBOARD_LED, OUTPUT);
   digitalWrite(PIN_ONBOARD_LED, HIGH);
 
   pixels.clear();
+  pixels.show();
+  delay(1000);
 
-  for (int i=0; i < PIXELS_N; i++) {
-    col = pixels.color(COLOR_LIST[i % COLOR_N][0],
-                       COLOR_LIST[i % COLOR_N][1],
-                       COLOR_LIST[i % COLOR_N][2]);
-    pixels.setColor(i, col);
-    pixels.show();
-    delay(LOOP_DELAY);
-  }
-
-  //delay(60000);
-  color_i = 0;
+  set_color_and_show(0, LOOP_DELAY);
 }
 
 /**
  *
  */
 void loop() {
+  static int color_i = 1;
+
   digitalWrite(PIN_ONBOARD_LED, LOW);
 
-  int color_i2 = color_i;
-  
-  for (int i=0; i < PIXELS_N; i++) {
-    uint32_t col = pixels.color(COLOR_LIST[color_i2][0],
-                                COLOR_LIST[color_i2][1],
-                                COLOR_LIST[color_i2][2]);
-    pixels.setColor(i, col);
-    color_i2 = (color_i2 + 1) % COLOR_N;
-  }
-  pixels.show();
-
+  set_color_and_show(color_i);
   color_i = (color_i + COLOR_N - 1) % COLOR_N;
 
   delay(LOOP_DELAY / 2);
