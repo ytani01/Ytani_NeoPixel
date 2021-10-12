6d2d9eca1e093b1dcd4fb2e29df50836a84d2f91
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-10-12 20:35:00 +0900

    ADD: tests/color_wave3

--- arduino-c++/tests/color_wave3/color_wave3.ino
+++ arduino-c++/tests/color_wave3/color_wave3.ino
@@ -1,11 +1,13 @@
 #include "Ytani_NeoPixel.h"
 
-const uint8_t PIN_ONBOARD_LED = 1;  // DigiSpark ATTiny85
-//const uint8_t PIN_ONBOARD_LED = 13;  // Arduino Uno
+const uint8_t PIN_ONBOARD_LED = 5;  // LOLIN D32 (ESP32)
+// const uint8_t PIN_ONBOARD_LED = 1;  // DigiSpark ATTiny85
+// const uint8_t PIN_ONBOARD_LED = 13;  // Arduino Uno
 
-const uint8_t PIN_PIXEL        = 4;
-const uint8_t PIXELS_N         = 255;
-const int     BRIGHTNESS       = 24;  // 1-255
+const uint8_t PIN_PIXEL[]    = {4};
+const uint8_t PIXELS_N       = sizeof(PIN_PIXEL) / sizeof(PIN_PIXEL[0]);
+const uint8_t LED_N          = 255;
+const int     BRIGHTNESS     = 32;  // 1-255
 
 const unsigned long LOOP_DELAY = 100;  // ms
 
@@ -26,7 +28,8 @@ const uint8_t COLOR_LIST[][3] =
   };
 const int COLOR_N = sizeof(COLOR_LIST) / sizeof(COLOR_LIST[0]);
 
-Ytani_NeoPixel pixels(PIXELS_N, PIN_PIXEL);
+Ytani_NeoPixel pixels(LED_N, PIN_PIXEL[0]);
+Ytani_NeoPixel *Pixel[PIXELS_N];
 
 /**
  *
@@ -36,7 +39,7 @@ void set_color_and_show(int color_i, unsigned long delay_msec=0) {
     color_i %= COLOR_N;
   }
 
-  for (int i=0; i < PIXELS_N; i++) {
+  for (int i=0; i < LED_N; i++) {
     pixels.setColor(i,
                     COLOR_LIST[color_i][0],
                     COLOR_LIST[color_i][1],
@@ -59,14 +62,31 @@ void set_color_and_show(int color_i, unsigned long delay_msec=0) {
  *
  */
 void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  Serial.printf("PIXELS_N=%d\n", PIXELS_N);
+
   pinMode(PIN_ONBOARD_LED, OUTPUT);
   digitalWrite(PIN_ONBOARD_LED, HIGH);
 
+  Pixel[0] = new Ytani_NeoPixel(LED_N, PIN_PIXEL[0]);
+  Serial.printf("AAA\n");
+  Pixel[0]->clear();
+  Pixel[0]->show();
+  Serial.printf("BBB\n");
+  delay(5000);
+  
+  Pixel[0]->setColor(0, 0xffffff);
+  Pixel[0]->setColor(1, 0xffff00);
+  Pixel[0]->show();
+  Serial.printf("CCC\n");
+  delay(5000);
+  
   pixels.clear();
   pixels.show();
   // delay(1000);
 
-  set_color_and_show(0, 10);
+  set_color_and_show(0, 5);
 }
 
 /**
