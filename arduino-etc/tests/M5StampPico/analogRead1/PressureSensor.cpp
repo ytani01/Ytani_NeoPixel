eb26ac70cdad4841dd37ea65397014cd03b370b6
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-11-18 23:10:21 +0900

    ADD: M5StampPico/analogRead1/

---
+++ arduino-etc/tests/M5StampPico/analogRead1/PressureSensor.cpp
@@ -0,0 +1,49 @@
+/**
+ * (c) 2021 Yoichi Tanibayashi
+ */
+#include "PressureSensor.h"
+
+/**
+ *
+ */
+unsigned long PressureSensor::bitWidth2maxVal(adc_bits_width_t width) {
+  unsigned long max_val = 0;
+  
+  switch (width) {  // XXX
+  case ADC_WIDTH_BIT_9:
+    max_val = 511;
+    break;
+  default:
+    max_val = 0;
+    break;
+  } // switch
+
+  return max_val;
+} // PressureSensor::bitWidth2maxVal()
+
+/**
+ * constructor
+ */
+PressureSensor::PressureSensor(adc1_channel_t adc_ch, adc_atten_t adc_atten,
+                               adc_bits_width_t width) {
+  this->_adc_ch = adc_ch;
+  this->_adc_atten = adc_atten;
+  this->_max_val = bitWidth2maxVal(width);
+  Serial.printf("%s> adc_ch=%d, adc_atten=%d, max_val=%d\n",
+                "PressureSensor::PressureSensor",
+                this->_adc_ch, this->_adc_atten, this->_max_val);
+  
+  adc1_config_width(width);
+  adc1_config_channel_atten(this->_adc_ch, this->_adc_atten);
+} // PressureSensor()
+
+/**
+ *
+ */
+int PressureSensor::get() {
+  esp_err_t ret;
+  int val;
+
+  val = adc1_get_raw(this->_adc_ch);
+  return (this->_max_val - val);
+} // PressureSensor::get()
---
+++ arduino-etc/tests/M5StampPico/analogRead1/Ytani_NeoPixel.h
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2021 Yoichi Tanibayashi
+//
+#ifndef YTANI_NEOPIXEL_H
+#define YTANI_NEOPIXEL_H
+
+#include <Adafruit_NeoPixel.h>
+
+/**
+ *
+ */
+class Ytani_NeoPixel {
+public:
+  Ytani_NeoPixel() {
+  }
+
+  Ytani_NeoPixel(uint8_t n, uint8_t pin) {
+    this->_pixels = Adafruit_NeoPixel(n, pin, NEO_GRB + NEO_KHZ800);
+    this->_pixels.begin();
+    this->_pixels.setBrightness(255);
+  }
+
+  void clear() {
+    this->_pixels.clear();
+  }
+
+  void show() {
+    this->_pixels.show();
+  }
+
+  void setColor(int i, uint32_t col) {
+    this->_pixels.setPixelColor(i, col);
+  }
+
+  void setColor(int i, uint8_t r, uint8_t g, uint8_t b) {
+    this->_pixels.setPixelColor(i, r, g, b);
+  }
+
+  uint32_t getColor(int i) {
+    return this->_pixels.getPixelColor(i);
+  }
+
+  uint32_t color(uint8_t r, uint8_t g, uint8_t b) {
+    return this->_pixels.Color(r, g, b);
+  }
+
+private:
+  Adafruit_NeoPixel _pixels;
+  uint8_t pixels_n = 0;
+  uint8_t pin;
+};
+#endif // YTANI_NEOPIXEL_H
