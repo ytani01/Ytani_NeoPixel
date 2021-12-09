6294ae25a454b8044d5bc55abcc6e065db679949
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-12-09 11:10:26 +0900

    UPD: switch3/

--- arduino-etc/ATTiny85/ATTiny85+CirleLEDs+CR2032/switch3/ModeBase.cpp
+++ arduino-etc/ATTiny85/ATTiny85+CirleLEDs+CR2032/switch3/ModeBase.cpp
@@ -7,3 +7,9 @@ ModeBase::ModeBase(Ytani_NeoPixel *leds, Button *btn) {
   this->_leds = leds;
   this->_btn  = btn;
 } // ModeBase()
+
+void ModeBase::loop() {
+}
+
+boolean ModeBase::btn_loop_hdr() {
+}
