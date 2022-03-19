e88a4f672c10dc9f73c1a0949d0985122bed5ab4
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-03-19 10:27:36 +0900

    START: LedLight4/

---
+++ arduino-etc/ATTiny85/ATTiny85+CirleLEDs+CR2032/LedLight4/Mode_Belt1.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2021 Yoichi Tanibayashi
+ */
+#include "Mode_Belt1.h"
+
+/**
+ *
+ */
+void Mode_Belt1::display(Ytani_NeoPixel *leds, bool force=false) {
+  uint16_t hue_deg = this->_cur_hue_deg;
+  for (int led_i=0; led_i < leds->pixels_n; led_i++) {
+    this->display1(leds, led_i, hue_deg);
+    if ( led_i % this->_belt_w == this->_belt_w - 1 ) {
+      hue_deg = (hue_deg + this->DEG_DIFF) % this->DEG_MAX;
+    }
+  } // for(led_i)
+}
