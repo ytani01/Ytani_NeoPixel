c0ed25ea45b6255b5c02772326e693b938a82eb1
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2023-07-01 19:30:04 +0900

    UPD

--- ATTiny85/LedTimer/LedTimer1/ModeTimer.h
+++ ATTiny85/LedTimer/LedTimer1/ModeTimer.h
@@ -8,12 +8,23 @@
 
 typedef unsigned long msec_t;
 
+/**
+ *
+ */
 class ModeTimer: public ModeBase {
 
 public:
+  static const msec_t TIME_LIMIT[] = {1, 2, 3, 4, 5,
+                                      10, 20, 30, 40, 50,
+                                      60, 120, 180, 240, 300};
+
+  /**
+   *
+   */
   ModeTimer(Ytani_NeoPixel *leds, Button *btn,
-              int eep_color_i=0, int eep_continuous=0)
+            int eep_color_i=0, int eep_continuous=0)
     : ModeBase(leds, btn, eep_color_i, eep_continuous) {
+
   } // constructor
 
   /**
@@ -21,13 +32,15 @@ public:
    */
   virtual void display(hue_deg_t hue_deg, sat_t sat) {
     for (int i=0; i < LEDS_N; i++) {
-      this->_leds->setColorHSVdeg(i, hue_deg, Ytani_NeoPixel::SAT_MAX, CurBr);
+      this->_leds->setColorHSVdeg(i, hue_deg, Ytani_NeoPixel::SAT_MAX,
+                                  CurBr);
+
       hue_deg = (hue_deg - 60) % 360;
-    }
+    } // for(i)
   } // display()
 
 private:
-  msec_t _timer;
+  int _time_limit_i;
   msec_t _time_left;
   
 }; // class ModeTimer
